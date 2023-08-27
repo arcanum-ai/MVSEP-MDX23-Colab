1665586fc78068dcf2134ce99c0fa27632f4d11c
alphatoaster <onixcontactmail@gmail.com>
2023-08-27 16:28:48 +0600

    Create gui.bat

---
+++ gui.bat
@@ -0,0 +1,19 @@
+:: Check for virtual environment
+if exist .venv\ (
+    .venv\Scripts\python.exe gui.py
+) else (
+    :: Check for python installation
+    python --version>NUL
+    if errorlevel 1 goto noPython
+
+    :: Setup virtualenv
+    python -m virtualenv .venv
+    .venv\Scripts\pip.exe install -r requirements.txt --index-url https://download.pytorch.org/whl/cu118
+    python gui.py
+    exit 0
+)
+:noPython
+echo.
+echo Error: Python not installed
+pause>nul
+exit 1
